1c09e670114c465d6d49c343eb350073edb6d14f
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-04-25 15:59:20 +0100

    simple get post

---
+++ main.py
@@ -0,0 +1,35 @@
+from typing import Optional, Annotated
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class STask(STaskAdd):
+    id: int
+
+tasks = []
+
+@app.post('/tasks')
+async def add_task(
+    task: Annotated[STaskAdd, Depends()]
+):
+    tasks.append(task)
+    return {'ok': True}
+
+
+# @app.get('/home')
+# def get_home():
+#     return {'data': 'Hello world'}
+#
+# @app.get('/tasks')
+# def get_tasks():
+#     task = Task(name='Write this video')
+#     return {'data': task}
